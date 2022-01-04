5ca96d5cccc27cdf1d7035d95901861670adb4d4
shwethanandikol <81556748+shwethanandikol@users.noreply.github.com>
2022-01-04 08:37:43 +0530

    Add files via upload

---
+++ TrainTester.java
@@ -0,0 +1,11 @@
+package com.xworkz.train;
+
+import com.xworkz.train.dto.TrainDto;
+
+public class TrainTester {
+public static void main(String[] args) {
+	TrainDto dto=new TrainDto(1,34215,"SBC","Kalaburgi",53,370,"Sleeper","UdyanExpress");
+	TrainDto dto1=new TrainDto(2,76549,"Kalaburgi","Hubli",43,265,"General","ScuenderbadExpress");
+	TrainDto dto2=new TrainDto(3,93654,"Vijayapur","Bangalore",28,300,"AC","GolgumbazExpress");
+}
+}
