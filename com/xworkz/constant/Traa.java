06bacc9a5011ad12c736441c16645bb45882b090
shwethanandikol <81556748+shwethanandikol@users.noreply.github.com>
2022-01-04 08:40:29 +0530

    Add files via upload

---
+++ com/xworkz/constant/Traa.java
@@ -0,0 +1,10 @@
+package com.xworkz.train.constant;
+
+import java.sql.DriverManager;
+
+public interface Traa {
+	String url = "jdbc:mysql://localhost:3306";
+	String username = "root";
+	String password = "1996";
+
+}
